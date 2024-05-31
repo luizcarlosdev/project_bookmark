74fd82c4be0ee8f8665f7d065fe3acaffa3515e3
Luiz <luiz.carlosdev@outlook.com>
2024-05-31 12:40:01 -0300

    cards de introducao finalizados

--- src/app/components/section_features/Features.tsx
+++ src/app/components/section_features/Features.tsx
@@ -41,23 +41,35 @@ export default function Features() {
                         <div className={Styles.features_list}>
                             <ul>
                                 <li className={Styles[(addEffectItem01 ? "add_item_effect" : "")]}
-                                onClick={isUsingEffectItem01}
+                                    onClick={isUsingEffectItem01}
                                 >simple bookmarking</li>
                                 <li className={Styles[(addEffectItem02 ? "add_item_effect" : "")]}
-                                onClick={isUsingEffectItem02}
+                                    onClick={isUsingEffectItem02}
                                 >speedy searching</li>
                                 <li className={Styles[(addEffectItem03 ? "add_item_effect" : "")]}
-                                onClick={isUsingEffectItem03}
+                                    onClick={isUsingEffectItem03}
                                 >easy sharing</li>
                             </ul>
                         </div>
                     </div>
-                    <Bookmarking 
-                    visibility={addEffectItem01}
-                    image="images/illustration-features-tab-1.svg"
-                    title='Book in one click'
-                    text='Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favorite sites.'
-                    textButton='More info'/>
+                    <Bookmarking
+                        visibility={addEffectItem01}
+                        image="images/illustration-features-tab-1.svg"
+                        title='Book in one click'
+                        text='Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favorite sites.'
+                        textButton='More info' />
+                    <Bookmarking
+                        visibility={addEffectItem02}
+                        image="images/illustration-features-tab-2.svg"
+                        title='Intelligent search'
+                        text='Our powerful search will help you find saved sites in nontime at all. No need to trawl through all of your bookmarks.'
+                        textButton='More info' />
+                    <Bookmarking
+                        visibility={addEffectItem03}
+                        image="images/illustration-features-tab-3.svg"
+                        title='Share your bookmarks'
+                        text='Easily share your bookmarks and collections with others. Create a shareble'
+                        textButton='More info' />
                 </div>
             </Container>
         </div>
